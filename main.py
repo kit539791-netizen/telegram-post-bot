8085606cd0183a963289b55c13e196a566174521
kit539791-netizen <kit539791@gmail.com>
2026-03-20 16:15:08 +0300

    main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import CommandStart
 
 from config import BOT_TOKEN, ADMIN_ID, CHANNEL_ID
-from db import add_post, get_post, update_status, get_user_posts
+from db import add_post, get_post, update_status, get_user_posts, cursor
 from keyboards import main_menu, moderation_kb
 
 bot = Bot(token=BOT_TOKEN)
@@ -13,30 +13,38 @@ dp = Dispatcher()
 @dp.message(CommandStart())
 async def start(message: types.Message):
     await message.answer(
-    "👋 Привет!\n\n"
-    "Здесь ты можешь разместить свой пост в нашем канале."
-    "📌 Как это работает:"
-    "1. Нажимаешь «ВЫЛОЖИТЬ ПОСТ»"
-    "2. Пишешь текст"
-    "3. Я отправляю его на модерацию"
-    "После проверки пост появится в канале 🚀",
-    reply_markup=main_menu()
-)
+        """👋 Привет!
+
+Здесь ты можешь разместить свой пост в нашем канале 🚀
+
+📌 Как это работает:
+1. Нажимаешь «ВЫЛОЖИТЬ ПОСТ»
+2. Пишешь текст
+3. Я отправляю его на модерацию
+
+После проверки пост появится в канале 👇
+""",
+        reply_markup=main_menu()
+    )
 
 
 @dp.message(F.text == "ВЫЛОЖИТЬ ПОСТ")
 async def create_post(message: types.Message):
     await message.answer(
-    "📋 Что должно быть в заявке:"
-     "• 📍 Адрес"
-     "• ⏰ Время выполнения работы"
-     "• 👷 Количество грузчиков"
-     "• 🧾 Описание задачи (что нужно сделать)"
-     "• 💰 Оплата (руб./час)"
+        """📋 Что должно быть в заявке:
 
-     "⚠️ Важно:"
-"Заявки без полного набора данных не рассматриваются и не публикуются."
-)
+📍 Адрес  
+⏰ Время выполнения работы  
+👷 Количество грузчиков  
+📦 Описание задачи (что нужно сделать)  
+💰 Оплата (руб./час)  
+
+⚠️ Важно:
+Заявки без полного набора данных
+не рассматриваются и не публикуются.
+"""
+    )
+
 
 @dp.message(F.text == "МОИ ПОСТЫ")
 async def my_posts(message: types.Message):
@@ -61,7 +69,7 @@ async def my_posts(message: types.Message):
 
 @dp.message(F.text == "ПОМОЩЬ")
 async def help_msg(message: types.Message):
-    await message.answer("По всем вопросам: @your_username")
+    await message.answer("📩 По всем вопросам: @your_username")
 
 
 @dp.message()
@@ -72,11 +80,11 @@ async def handle_post(message: types.Message):
 
     await bot.send_message(
         ADMIN_ID,
-        f"Новый пост:\n\n{text}",
+        f"📨 Новый пост на модерацию:\n\n{text}",
         reply_markup=moderation_kb(post_id)
     )
 
-    await message.answer("Пост отправлен на модерацию!")
+    await message.answer("⏳ Пост отправлен на модерацию!")
 
 
 @dp.callback_query(F.data.startswith("approve_"))
@@ -104,13 +112,11 @@ async def approve(callback: types.CallbackQuery):
 async def reject(callback: types.CallbackQuery):
     post_id = int(callback.data.split("_")[1])
 
-    # получаем пользователя
     cursor.execute("SELECT user_id FROM posts WHERE id=?", (post_id,))
     user_id = cursor.fetchone()[0]
 
     update_status(post_id, "rejected")
 
-    # уведомление пользователю
     await bot.send_message(user_id, "❌ Твой пост отклонён модератором")
 
     await callback.message.edit_text("❌ Отклонено")
